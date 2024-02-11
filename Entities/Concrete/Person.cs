97e089214bb58629b11793717670d2c46ce5777c
FARUK UZUN <farukbirkanuzun@gmail.com>
2024-02-11 19:17:21 +0300

    Add project files.

---
+++ Entities/Concrete/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public long NationalIdentity { get; set; }
+        public string NationalBirthYear { get; set; }
+
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SelamVer("Faruk");
+            SelamVer( );
+
+            
+
+            Console.Write("1. Sayıyı giriniz: ");
+            int s1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("2. Sayıyı giriniz: ");
+            int s2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Topla(s1, s2);
+            // Console.WriteLine("Hello World!");
+        }
+
+
+        static void  SelamVer(string isim="DefaultParametre")
+        {
+            Console.WriteLine("Merhaba  : "+ isim);
+        }
+
+        static int Topla(int sayi1 ,int sayi2)
+        { 
+            int sonuc=sayi1 + sayi2;
+            Console.WriteLine("Toplam Sonuc : " + sonuc);
+            return sonuc;
+        }
+
+     
+    }
+}
